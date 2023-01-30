c1b7c93f57286d96a28844c468797bcbef6bfe14
Arjun <sunsignsagittarius@gmail.com>
2023-01-30 14:50:41 +0545

    Worked On React-Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 
 import './App.css';
-import Header from './Components/Header/header';
 import Footer from './Components/Footer/footer';
-import Main from './Components/Main/main';
 import IconsFontsImg from './Components/Icons-fonts-img/IconsFontsImg';
+import Page from './Pages/Page';
 //function name then its location
 
 
 function App() {
   return (
-   <>
-   <h1 style={{color:"white",textAlign:"center", fontSize:"44px"}}>SetUp</h1>
-   <Header/>
-   <Main/>
-   <IconsFontsImg/>
-   <Footer/>
-   </>
+    <>
+      <Page />
+      <IconsFontsImg />
+      <Footer />
+
+
+    </>
   );
 }
 
